e3e3a3bb1a550cb675fa45af572559eb0aba7dac
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-09-25 16:26:21 +0300

    fix: removed unused test

--- libs/shared/util-interface/src/lib/util-interface.spec.ts
+++
@@ -1,7 +0,0 @@
-import { utilInterface } from './util-interface';
-
-describe('utilInterface', () => {
-  it('should work', () => {
-    expect(utilInterface()).toEqual('util-interface');
-  });
-});
