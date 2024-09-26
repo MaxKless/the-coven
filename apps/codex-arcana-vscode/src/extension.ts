d64437e9f3e24001ab1a4215c7f65f518ac1e1a2
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-09-26 16:54:44 +0300

    feat: added view

--- apps/codex-arcana-vscode/src/extension.ts
+++ apps/codex-arcana-vscode/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
+import { initSpellcastingView } from './spellcasting-view';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -15,13 +16,15 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "codex-arcana-vscode.helloWorld",
+    'codex-arcana-vscode.helloWorld',
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Sup, witches");
+      vscode.window.showInformationMessage('Sup, witches');
     }
   );
+
+  initSpellcastingView();
 
   context.subscriptions.push(disposable);
 }
