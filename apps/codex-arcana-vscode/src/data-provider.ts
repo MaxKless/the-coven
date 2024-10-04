cb23f641e63830f9ce126a46f578b0e04225f247
Max Kless <maxk@nrwl.io>
2024-10-04 09:30:35 -0400

    treeview works (kinda)

--- apps/codex-arcana-vscode/src/data-provider.ts
+++ apps/codex-arcana-vscode/src/data-provider.ts
@@ -1,3 +1,4 @@
+import { SpellCastingSDK } from '@the-coven/spellcasting-sdk';
 import { Recipe } from '@the-coven/util-interface';
 import {
   CancellationToken,
@@ -5,6 +6,7 @@ import {
   ProviderResult,
   TreeDataProvider,
   TreeItem,
+  TreeItemCollapsibleState,
 } from 'vscode';
 
 export class SpellcastingTreeDataProvider
@@ -17,15 +19,44 @@ export class SpellcastingTreeDataProvider
   getTreeItem(element: TreeItem): TreeItem | Thenable<TreeItem> {
     return element;
   }
-  getChildren(element?: TreeItem | undefined): ProviderResult<TreeItem[]> {
+  async getChildren(
+    element?: TreeItem | undefined
+  ): Promise<TreeItem[] | undefined> {
     if (!element) {
+      const sdk = new SpellCastingSDK('https://the-coven.vercel.app');
+      this.ingredients = await sdk.getAllIngredients();
+      this.incantations = await sdk.getAllIncantations();
+      this.recipes = await sdk.getAllRecipes();
+
       return [
         {
           label: 'Ingredients',
+          collapsibleState: TreeItemCollapsibleState.Collapsed,
         },
-        { label: 'Incantations' },
-        { label: 'Recipes' },
+        {
+          label: 'Incantations',
+          collapsibleState: TreeItemCollapsibleState.Collapsed,
+        },
+        {
+          label: 'Recipes',
+          collapsibleState: TreeItemCollapsibleState.Collapsed,
+        },
       ];
+    }
+    if (element.label === 'Ingredients') {
+      return this.ingredients.map((ingredient) => ({
+        label: ingredient,
+      }));
+    }
+    if (element.label === 'Incantations') {
+      return this.incantations.map((incantation) => ({
+        label: incantation,
+      }));
+    }
+    if (element.label === 'Recipes') {
+      return this.recipes.map((recipe) => ({
+        label: recipe.name,
+      }));
     }
   }
 }
