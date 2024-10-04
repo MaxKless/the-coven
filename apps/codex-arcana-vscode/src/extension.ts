02c1c4cebb5cc083375e352600b41a1a9337d6f5
Max Kless <maxk@nrwl.io>
2024-10-04 09:46:04 -0400

    copy command

--- apps/codex-arcana-vscode/src/extension.ts
+++ apps/codex-arcana-vscode/src/extension.ts
@@ -17,6 +17,13 @@ export function activate(context: vscode.ExtensionContext) {
     new SpellcastingTreeDataProvider()
   );
 
+  vscode.commands.registerCommand(
+    'codex-arcana-vscode.copy',
+    (item: vscode.TreeItem) => {
+      vscode.env.clipboard.writeText(item.label?.toString() || '');
+    }
+  );
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
